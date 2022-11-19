c4dc8f45e7eeed0bde1daa6f829d9e9a15d81e92
parsapanahpoor <parsapanahpoor77@gmail.com>
2022-11-19 23:27:21 +0330

    intiial admin area

---
+++ DoctorType.Web/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DoctorType.Web.Areas.Admin.Controllers
+{
+    public class HomeController : AdminBaseController
+    {
+        #region Constructor
+
+        //private readonly IAdminHomeIndexService _adminHomeIndex;
+        //private readonly IAdvertisementService _advertisementService;
+        //private readonly IAdsCategoryService _adsCategoryService;
+        //public HomeController(IAdminHomeIndexService adminHomeIndex, IAdvertisementService advertisementService
+        //    , IAdsCategoryService adsCategoryService)
+        //{
+        //    _adminHomeIndex = adminHomeIndex;
+        //    _advertisementService = advertisementService;
+        //    _adsCategoryService = adsCategoryService;
+        //}
+
+        #endregion
+
+        #region Admin Dashboard
+
+        public async Task<IActionResult> Index()
+        {
+            //var model = await _adminHomeIndex.SetAdminHomeIndexViewModel();
+            return View();
+        }
+
+        #endregion
+
+    }
+}
